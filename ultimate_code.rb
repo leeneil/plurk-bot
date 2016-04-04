9c818e1e8a026f148c39b4a1e54f1d24c51b0b74
Po-Nan Li <lishantreasure@gmail.com>
2016-04-04 15:22:50 -0700

    migrate responses to JSON

--- ultimate_code.rb
+++ ultimate_code.rb
@@ -21,41 +21,8 @@ last_time = Time.now.utc
 code_keywords = ["終極密碼"]
 aabb_keywords = ["猜數字"]
 # keywords = ["每日一冷", "冷知識", "你知道嗎", "阿冷","難過","還是會","下雨","那一年","那些年","無言","掰噗","林怡","芋頭","盧董","五月天"]
-responses = {"每日一冷"=>"今天好熱，你每日一冷了嗎？ (sick) \n  www.dailycold.tw/?redirect_to=random (本日冷知識)", \
-	"冷知識"=>"想找 www.dailycold.tw/?redirect_to=random (冷知識)？問阿冷就對惹  (griltongue) \n 你知道嗎？ ", \
-	"你知道嗎"=>"你知道嗎？ (woot) \n www.dailycold.tw/?redirect_to=random (本日冷知識)", \
-	"那一年"=>"默默無言只能選擇離開 :-( \n https://www.youtube.com/watch?v=2Ii0kpKi8kI", \
-	"那些年"=>"戳鍋der大魚 \n https://www.youtube.com/watch?v=xWzlwGVQ6_Q", \
-	"難過"=>"的是？ (cozy) \n https://www.youtube.com/watch?v=2Ii0kpKi8kI", \
-	"無言"=>"只能選擇離開  (tears)\n https://www.youtube.com/watch?v=2Ii0kpKi8kI", \
-	"56"=>"56不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
-	"五六"=>"五六不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
-	"5566"=>"56不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
-	"五五六六"=>"五六不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
-	"還是會"=>"害怕  :-o \n https://www.youtube.com/watch?v=eGNqW9sybyU", \
-	"下雨天"=>"下雨天了怎麼辦我好想你 \n https://www.youtube.com/watch?v=HYFJcEJ20KU",\
-	"下雨"=>"的聲音 \n https://www.youtube.com/watch?v=F2uX6ByoW7A", \
-	"下大雨"=>"九索！ (yay)",\
-	"掰噗"=>"阿冷希望有一天也可以成為跟掰噗一樣的機器人！ (droid_dance)", 
-	"林怡"=>"01 是每日一冷的女神 (gyay)", \
-	"盧董"=>"盧董以前可是很瘦的 (aha)", \
-	"芋頭"=>"阿冷最討厭吃芋頭了QQ (bah)",\
-	"Mai"=>"Maii是正妹",\
-	"五月天"=>"突然好想你 你會在哪裡？\n https://www.youtube.com/watch?v=GtDRcXtDg-4",\
-	"佐藤光"=>"佐藤光是阿亮最大的對手",\
-	"塔矢亮"=>"塔矢亮是小光最大的對手",\
-	"sai"=>"sai只是個軟體 (bah)",\
-	"alphago"=>"alphago 只是個軟體 (LOL)",\
-	"初音"=>"初音只是個軟體 (troll)",\
-	"真的"=>"真的假不了 (woot)",\
-	"跑跑"=>"跑跑卡丁車 (woot)",\
-	"噗噗"=>"did you mean: **噗浪**？",\
-	"噗浪"=>"我愛噗浪 噗浪愛我 <3",\
-	"好冷"=>"對不起QQ",\
-	"好熱"=>"那來點 www.dailycold.tw/?redirect_to=random (冷知識) 吧 (heart) \n ",\
-	"飲冰室茶集"=>"好兆，明天準是好天了。",\
-	"睡著"=>"我在想你的時候睡著惹 (v_love) \n https://www.youtube.com/watch?v=BqUH8X4hTGA",\
-	"阿冷"=>"安安，我是阿冷 (wave) \n 在你的河道上噗**冷知識**我會提供一則冷知識給您 \n 噗**終極密碼**或**猜數字**可以跟阿冷玩遊戲唷！  (goodluck)"}
+
+responses = JSON.parse( open("responses.json").read )
 
 # build keyword list
 keywords = []
@@ -102,6 +69,7 @@ while true
 		# puts "no new plurks" + " @ " + Time.now.strftime("%Y-%m-%dT%H:%M:%S")
 	else
 		for p in plurks
+			replied = false
 			pid = p["plurk_id"]
 			puts "@" + users[ p["owner_id"].to_s ]["nick_name"] \
 				+ " \\ " + users[ p["owner_id"].to_s ]["display_name"]
@@ -122,7 +90,8 @@ while true
 						plurk.post("/APP/Responses/responseAdd", \
 							{:plurk_id=>pid, \
 							:content=>"遊戲開始！ 0~9999 (code)", \
-							:qualifier=>"is"})	
+							:qualifier=>"is"})
+						replied = true			
 
 					end
 				end
@@ -130,7 +99,7 @@ while true
 			# AABB game
 			for keyword in aabb_keywords
 				key_match = p["content_raw"].match(keyword)
-				unless key_match.nil?
+				unless key_match.nil? or replied
 					puts "Keyword " + keyword + " identified!"
 	
 					if aabb_sessions[ pid ].nil?
@@ -142,13 +111,14 @@ while true
 						plurk.post("/APP/Responses/responseAdd", \
 							{:plurk_id=>pid, \
 							:content=>"遊戲開始！ 1000~9999 (code_okok)", \
-							:qualifier=>"is"})	
+							:qualifier=>"is"})
+						replied = true	
 
 					end
 				end
 			end
 			# otherwise search for other keywords
-			replied = false
+			
 			for keyword in keywords
 				key_match = p["content"].match(keyword)
 				unless key_match.nil? or  replied
@@ -159,7 +129,7 @@ while true
 						{:plurk_id=>pid, \
 						:content=>msg, \
 						:qualifier=>"says"})
-				replied = true			
+					replied = true			
 				end
 			end
 			# mark as read
