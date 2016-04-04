a669357e8d9a3266a4cb5843aabf6af71933fe42
Po-Nan Li <lishantreasure@gmail.com>
2016-04-04 00:39:17 -0700

    auto create keyowrd list

--- ultimate_code.rb
+++ ultimate_code.rb
@@ -18,20 +18,46 @@ plurk.authorize(ACCESS_TOKEN, \
 last_time = Time.now.utc
 
 game_keywords = ["終極密碼"]
-keywords = ["每日一冷", "冷知識", "你知道嗎", "阿冷","難過","還是會","下雨","那一年","無言","掰噗","林怡","芋頭","盧董"]
-responses = {"每日一冷"=>"今天好熱，你每日一冷了嗎？ (sick) \n 你知道嗎？ www.dailycold.tw/?redirect_to=random ", \
-	"冷知識"=>"想找冷知識？問阿冷就對惹  (griltongue) \n 你知道嗎？ www.dailycold.tw/?redirect_to=random (本日冷知識)", \
-	"你知道嗎"=>"你知道嗎？ (woot) \n 你知道嗎？ www.dailycold.tw/?redirect_to=random ", \
+# keywords = ["每日一冷", "冷知識", "你知道嗎", "阿冷","難過","還是會","下雨","那一年","那些年","無言","掰噗","林怡","芋頭","盧董","五月天"]
+responses = {"每日一冷"=>"今天好熱，你每日一冷了嗎？ (sick) \n  www.dailycold.tw/?redirect_to=random (本日冷知識)", \
+	"冷知識"=>"想找 www.dailycold.tw/?redirect_to=random (冷知識)？問阿冷就對惹  (griltongue) \n 你知道嗎？ ", \
+	"你知道嗎"=>"你知道嗎？ (woot) \n www.dailycold.tw/?redirect_to=random (本日冷知識)", \
 	"阿冷"=>"安安，我是阿冷 (wave) \n 在你的河道上噗**冷知識**我會提供一則冷知識給您 \n 噗**終極密碼**可以跟阿冷玩遊戲唷！  (goodluck)", \
 	"那一年"=>"默默無言只能選擇離開 :-( \n https://www.youtube.com/watch?v=2Ii0kpKi8kI", \
+	"那些年"=>"戳鍋der大魚 \n https://www.youtube.com/watch?v=xWzlwGVQ6_Q", \
 	"難過"=>"的是？ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI", \
 	"無言"=>"只能選擇離開  (tears)\n https://www.youtube.com/watch?v=2Ii0kpKi8kI", \
+	"56"=>"56不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
+	"五六"=>"五六不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
+	"5566"=>"56不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
+	"五五六六"=>"五六不能亡！ \n https://www.youtube.com/watch?v=2Ii0kpKi8kI",\
 	"還是會"=>"害怕  :-o \n https://www.youtube.com/watch?v=eGNqW9sybyU", \
+	"下雨天"=>"下雨天了怎麼辦我好想你 \n https://www.youtube.com/watch?v=HYFJcEJ20KU",\
 	"下雨"=>"的聲音 \n https://www.youtube.com/watch?v=F2uX6ByoW7A", \
+	"下大雨"=>"九索！",\
 	"掰噗"=>"阿冷希望有一天也可以成為跟掰噗一樣的機器人！", 
 	"林怡"=>"01 是每日一冷的女神 (worship)", \
 	"盧董"=>"盧董以前可是很瘦的", \
-	"芋頭"=>"阿冷最討厭吃芋頭了QQ"}
+	"芋頭"=>"阿冷最討厭吃芋頭了QQ",\
+	"五月天"=>"突然好想你 你會在哪裡？\n https://www.youtube.com/watch?v=GtDRcXtDg-4",\
+	"佐藤光"=>"佐藤光是阿亮最大的對手",\
+	"塔矢亮"=>"塔矢亮是小光最大的對手",\
+	"sai"=>"sai只是個軟體 (LOL)",\
+	"alphago"=>"alphago 只是個軟體 (LOL)",\
+	"初音"=>"初音只是個軟體 (goodluck)",\
+	"真的"=>"真的假不了 (woot)",\
+	"跑跑"=>"跑跑卡丁車 (woot)",\
+	"噗噗"=>"did you mean: **噗浪**？",\
+	"噗浪"=>"我愛噗浪 噗浪愛我 <3",\
+	"好冷"=>"對不起QQ",\
+	"好熱"=>"那來點 www.dailycold.tw/?redirect_to=random (冷知識) 吧 (heart) \n ",\
+	"飲冰室茶集"=>"好兆，明天準是好天了。"}
+
+# build keyword list
+keywords = []
+responses.each do |key, value|
+	keywords << key
+end
 
 play_sessions = {}
 
