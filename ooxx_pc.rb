b7adde11b96cdfb4ffdb7fbb2b340bcb41a814db
Po-Nan Li <lishantreasure@gmail.com>
2016-04-08 00:33:25 -0700

    adjust difficulty

--- ooxx_pc.rb
+++ ooxx_pc.rb
@@ -2,37 +2,46 @@ def ooxx_pc(game, player)
 
 	require "./ooxx_score"
 
-	# random step
-	# move = rand(9)
-	# while game[move] != '_'
-	# 	move = rand(9)
-	# end
-	# game[move] = player
-	# return game
+	hard = 0.6
 
-	# best step
-	if player == 'O'
-		opp = 'X'
+	if rand > hard
+
+		# random step
+		puts "[random step]"
+		move = rand(9)
+		while game[move] != '_'
+			move = rand(9)
+		end
+		game[move] = player
+		return game
+
 	else
-		opp = 'O'
-	end
-	best_step = 0
-	best_score = 0
-	for u in 0..8
-		score = 0
-		if game[u] == '_'
-			tmp1 = game.dup
-			tmp1[u] = player
-			tmp2 = game.dup
-			tmp2[u] = opp
-			score = ooxx_score(tmp1, player) + 0.5*ooxx_score(tmp2, opp)
-			if score > best_score
-				best_score = score
-				best_step = u
+
+		# best step
+		if player == 'O'
+			opp = 'X'
+		else
+			opp = 'O'
+		end
+		best_step = 0
+		best_score = 0
+		for u in 0..8
+			score = 0
+			if game[u] == '_'
+				tmp1 = game.dup
+				tmp1[u] = player
+				tmp2 = game.dup
+				tmp2[u] = opp
+				score = ooxx_score(tmp1, player) + 0.5*ooxx_score(tmp2, opp)
+				if score > best_score
+					best_score = score
+					best_step = u
+				end
 			end
 		end
+		game[best_step] = player
+		return game
+
 	end
-	game[best_step] = player
-	return game
 
 end
